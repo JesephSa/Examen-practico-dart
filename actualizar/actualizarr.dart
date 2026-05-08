849972af687fe9e282183b12e2a8407d4f23de9c
ari4ncito <ari.sv.1806@gmail.com>
2026-05-08 07:43:49 -0500

    feat: se termino actualizar

--- actualizar/actualizarr.dart
+++ actualizar/actualizarr.dart
@@ -1,16 +1,35 @@
 //actualizar
 import 'dart:io';
-
-
+import '../main.dart';
+import '../Crear/crear.dart';
 
 void actualizarr() {
 
-  print("actualizar productos");
+  print("actualizar producto");
+  print("ingrese el numero del producto a actualizar;");
+  int numProducto = int.parse(stdin.readLineSync()!);
 
-  if (i > 0) {
+  if (numProducto > 0 ) {
+    
+    print("ingrese el nuevo nombre del producto:");
+    String nuevoNombre = stdin.readLineSync()!;
+    nombre[numProducto - 1] = nuevoNombre;
 
-    print("ingrese el numero del producto a actualizar;");
+    print("ingrese el nuevo precio del producto:");
+    double nuevoPrecio = double.parse(stdin.readLineSync()!);
+    precio[numProducto - 1] = nuevoPrecio;
 
+    print("ingrese la nueva cantidad del producto:");
+    int nuevaCantidad = int.parse(stdin.readLineSync()!);
+    cantidad[numProducto - 1] = nuevaCantidad;
+
+  } else {
+    print("numero de producto no valido");
   }
+
+
+
+
+  
 
 }
--- main.dart
+++ main.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 
-
 List<String> productos = [];
 void main() {
   int opcion;
